23b66946a5f4c85c7b7b26f6713f3d32df657548
maria jose <majog9714@gmail.com>
2021-06-10 19:29:00 -0500

    acabada clase 13, se agregan documentos de clase 14

---
+++ 05FundamentosJS/js/fundamentos2.js
@@ -0,0 +1,13 @@
+//DECLARACION DE OBJETOS Y VARIABLES
+var boton = document.getElementById("boton");
+var boton2 = document.getElementById("boton2");
+
+//DECLARACION DE FUNCIONES
+function eventoClick(){
+  alert("Has presionado el botón");
+}
+//ASIGNACIÓN DE EVENTOS
+//los manejadores de eventos semanticos se ejecutan a la carga del documento
+window.onload = function(){
+  boton.onclick= eventoClick;
+}
